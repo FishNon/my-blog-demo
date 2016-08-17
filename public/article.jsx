6cfe63c3526579db5bf0b1491cac6393b7f6a716
FishNon <757505375@qq.com>
2016-08-17 14:57:49 +0800

    article-part2

--- public/article.jsx
+++ public/article.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import request from 'superagent';
 export default class Article extends Component {
     constructor(props) {
         super(props);
@@ -10,7 +10,7 @@ export default class Article extends Component {
     }
 
     render() {
-        return <form>
+        return <div>
             <div>
                 <input type="text" placeholder="title"
                        value={this.state.title}
@@ -22,14 +22,9 @@ export default class Article extends Component {
                           onChange={this._onContentChange.bind(this)}/>
             </div>
             <div>
-                <button>提交</button>
+                <button type="submit" onClick={this._onSubmit.bind(this)}>提交</button>
             </div>
-            <div>
-                <hr/>
-                {this.state.title}
-                {this.state.content}
-            </div>
-        </form>
+        </div>
     }
 
     _onTitleChange(event) {
@@ -42,5 +37,18 @@ export default class Article extends Component {
         this.setState({
             content: event.target.value
         })
+    }
+
+    _onSubmit() {
+        alert('sda');
+        request.post('api/article')
+            .send({
+                title: this.state.title,
+                content: this.state.content
+            })
+            .end((err, res)=> {
+                if (err) return console.err(err);
+                console.log(res.statusCode);
+            })
     }
 }
